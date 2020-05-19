3ca065706acb717b83907d157eb7fcd77a9ce377
Iosu <lizarraga.iosu@gmail.com>
2020-05-19 11:41:35 +0200

    [WIP] update BaseApplication

--- app/src/main/java/com/ilizma/eztandairratiapp/application/BaseApplication.kt
+++ app/src/main/java/com/ilizma/eztandairratiapp/application/BaseApplication.kt
@@ -1,18 +1,23 @@
 package com.ilizma.eztandairratiapp.application
 
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.ProcessLifecycleOwner
 import cat.ereza.customactivityoncrash.config.CaocConfig
 import com.ilizma.eztandairratiapp.di.component.DaggerAppComponent
 import com.ilizma.presentation.ui.customview.EztandaCrashActivity
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
+import timber.log.Timber
 
-class BaseApplication : DaggerApplication() {
+class BaseApplication : DaggerApplication(), LifecycleObserver {
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
         DaggerAppComponent.builder().application(this).build()
 
     override fun onCreate() {
         super.onCreate()
+        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
+        Timber.plant(Timber.DebugTree())
         CaocConfig.Builder.create()
             .errorActivity(EztandaCrashActivity::class.java)
             .apply()
