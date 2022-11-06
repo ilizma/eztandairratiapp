6f8dff55400308a82d3caf38ad7751688957583d
Iosu <lizarraga.iosu@gmail.com>
2022-11-06 17:09:50 +0100

    Bump version 2.1.0

--- buildSrc/src/main/kotlin/ConfigData.kt
+++ buildSrc/src/main/kotlin/ConfigData.kt
@@ -2,8 +2,8 @@ object ConfigData {
     //android
     const val minSdkVersion = 21
     const val targetSdk = 33
-    const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionCode = 2
+    const val versionName = "2.1.0"
     const val compileSdkVersion = 33
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 }
