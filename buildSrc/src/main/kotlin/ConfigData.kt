8dea3adc65b7461c13bc0c13a68b6095dc24dc5f
iosu.lizarraga <iosu.lizarraga@cabify.com>
2023-09-04 15:59:36 +0200

    v3.0.1

--- buildSrc/src/main/kotlin/ConfigData.kt
+++ buildSrc/src/main/kotlin/ConfigData.kt
@@ -5,8 +5,8 @@ object ConfigData {
     const val minSdkVersion = 21
     const val targetSdk = 34
     const val compileSdkVersion = 34
-    const val versionCode = 4
-    const val versionName = "3.0.0"
+    const val versionCode = 5
+    const val versionName = "3.0.1"
     const val kotlinCompilerExtensionVersion = "1.5.1"
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     val javaVersion = JavaVersion.VERSION_17
