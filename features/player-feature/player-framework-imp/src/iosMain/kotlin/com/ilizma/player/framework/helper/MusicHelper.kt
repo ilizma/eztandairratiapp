2a9ff4c0dbdfaaf41dd2a70c0f99a31130b7a4f9
Iosu <lizarraga.iosu@gmail.com>
2025-04-12 16:54:19 +0200

    fix iOS player adding http

--- features/player-feature/player-framework-imp/src/iosMain/kotlin/com/ilizma/player/framework/helper/MusicHelper.kt
+++ features/player-feature/player-framework-imp/src/iosMain/kotlin/com/ilizma/player/framework/helper/MusicHelper.kt
@@ -1,21 +1,40 @@
 package com.ilizma.player.framework.helper
 
 import com.ilizma.player.framework.imp.BuildKonfig
-import kotlinx.cinterop.*
+import kotlinx.cinterop.CValue
+import kotlinx.cinterop.ExperimentalForeignApi
 import platform.AVFAudio.AVAudioSession
 import platform.AVFAudio.AVAudioSessionCategoryPlayback
+import platform.AVFAudio.AVAudioSessionPortOverrideSpeaker
 import platform.AVFAudio.setActive
-import platform.AVFoundation.*
+import platform.AVFoundation.addPeriodicTimeObserverForInterval
+import platform.AVFoundation.AVPlayer
+import platform.AVFoundation.AVPlayerTimeControlStatusPlaying
+import platform.AVFoundation.AVPlayerItemPlaybackStalledNotification
+import platform.AVFoundation.currentItem
+import platform.AVFoundation.isPlaybackLikelyToKeepUp
+import platform.AVFoundation.removeTimeObserver
+import platform.AVFoundation.pause
+import platform.AVFoundation.play
+import platform.AVFoundation.timeControlStatus
 import platform.CoreMedia.CMTime
 import platform.CoreMedia.CMTimeMakeWithSeconds
-import platform.Foundation.*
+import platform.Foundation.NSNotificationCenter
+import platform.Foundation.NSOperationQueue
+import platform.Foundation.NSURL
 import platform.darwin.NSEC_PER_SEC
+import platform.MediaPlayer.MPMediaItemPropertyArtist
+import platform.MediaPlayer.MPMediaItemPropertyTitle
+import platform.MediaPlayer.MPNowPlayingInfoCenter
+import platform.MediaPlayer.MPNowPlayingInfoPropertyPlaybackRate
 
 class MusicHelper {
 
     private val player: AVPlayer = BuildKonfig.AUDIO_URL
         .let { NSURL.URLWithString(URLString = it)!! }
         .let { AVPlayer(it) }
+
+    private var session: AVAudioSession? = null
 
     private var timeObserver: Any? = null
 
@@ -34,27 +53,24 @@ class MusicHelper {
         stop()
         startTimeObserver(observer = observer)
         showNotification()
-        BuildKonfig.AUDIO_URL
-            .let { NSURL.URLWithString(URLString = it) }
-            ?.let { AVPlayerItem(uRL = it) }
-            ?.let { player.replaceCurrentItemWithPlayerItem(item = it) }
+        updateNowPlayingInfo(
+            title = "Eztanda Irratia",
+            artist = "107.6 FM",
+        )
         player.play()
-    }
-
-    fun pause() {
-        player.pause()
     }
 
     fun stop() {
         timeObserver
             ?.let { player.removeTimeObserver(observer = it) }
+        timeObserver = null
         player.pause()
     }
 
     @OptIn(ExperimentalForeignApi::class)
     private fun setUpAudioSession() {
         try {
-            AVAudioSession.sharedInstance()
+            session = AVAudioSession.sharedInstance()
                 .apply {
                     setCategory(
                         category = AVAudioSessionCategoryPlayback,
@@ -62,6 +78,10 @@ class MusicHelper {
                     )
                     setActive(
                         active = true,
+                        error = null,
+                    )
+                    overrideOutputAudioPort(
+                        portOverride = AVAudioSessionPortOverrideSpeaker,
                         error = null,
                     )
                 }
@@ -86,13 +106,33 @@ class MusicHelper {
         }
     }
 
+    @OptIn(ExperimentalForeignApi::class)
     private fun showNotification() {
         NSNotificationCenter.defaultCenter.addObserverForName(
             name = AVPlayerItemPlaybackStalledNotification,
             `object` = player.currentItem,
             queue = NSOperationQueue.mainQueue,
-            usingBlock = { stop() },
+            usingBlock = {
+                stop()
+                session?.setActive(
+                    active = false,
+                    error = null,
+                )
+            },
         )
+    }
+
+    private fun updateNowPlayingInfo(
+        title: String,
+        artist: String,
+    ) {
+        val nowPlayingInfo = mutableMapOf<Any?, Any?>(
+            MPMediaItemPropertyTitle to title,
+            MPMediaItemPropertyArtist to artist,
+            MPNowPlayingInfoPropertyPlaybackRate to 1.0
+        )
+
+        MPNowPlayingInfoCenter.defaultCenter().nowPlayingInfo = nowPlayingInfo
     }
 }
 
