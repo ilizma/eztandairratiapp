e19bedf67a80222ea4976d004b5318dc92d66ffd
Iosu <lizarraga.iosu@gmail.com>
2025-11-18 20:35:34 +0100

    [RELEASE] Android 3.4.0 + iOS 1.2.0

--- buildSrc/src/main/kotlin/ConfigData.kt
+++ buildSrc/src/main/kotlin/ConfigData.kt
@@ -2,8 +2,8 @@ import org.gradle.api.JavaVersion
 
 object ConfigData {
     //android
-    const val versionCode = 9
-    const val versionName = "3.3.0"
+    const val versionCode = 10
+    const val versionName = "3.4.0"
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     val javaVersion = JavaVersion.VERSION_21
 }
