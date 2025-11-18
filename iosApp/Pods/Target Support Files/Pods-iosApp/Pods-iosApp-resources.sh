b68c52b155abdcf6011325af3b1a952bb84be25c
Iosu <lizarraga.iosu@gmail.com>
2025-11-18 20:21:24 +0100

    iOS updates

--- iosApp/Pods/Target Support Files/Pods-iosApp/Pods-iosApp-resources.sh
+++ iosApp/Pods/Target Support Files/Pods-iosApp/Pods-iosApp-resources.sh
@@ -106,6 +106,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "${PODS_CONFIGURATION_BUILD_DIR}/GoogleUtilities/GoogleUtilities_Privacy.bundle"
   install_resource "${PODS_CONFIGURATION_BUILD_DIR}/PromisesObjC/FBLPromises_Privacy.bundle"
   install_resource "${PODS_CONFIGURATION_BUILD_DIR}/PromisesSwift/Promises_Privacy.bundle"
+  install_resource "${PODS_ROOT}/../../composeApp/build/compose/cocoapods/compose-resources"
   install_resource "${PODS_CONFIGURATION_BUILD_DIR}/nanopb/nanopb_Privacy.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
@@ -118,6 +119,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "${PODS_CONFIGURATION_BUILD_DIR}/GoogleUtilities/GoogleUtilities_Privacy.bundle"
   install_resource "${PODS_CONFIGURATION_BUILD_DIR}/PromisesObjC/FBLPromises_Privacy.bundle"
   install_resource "${PODS_CONFIGURATION_BUILD_DIR}/PromisesSwift/Promises_Privacy.bundle"
+  install_resource "${PODS_ROOT}/../../composeApp/build/compose/cocoapods/compose-resources"
   install_resource "${PODS_CONFIGURATION_BUILD_DIR}/nanopb/nanopb_Privacy.bundle"
 fi
 
