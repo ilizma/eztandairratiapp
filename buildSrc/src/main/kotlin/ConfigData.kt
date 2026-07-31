4dcff45c0cfd56f9fc3c06c303d76e388a4fdcce
Iosu <lizarraga.iosu@gmail.com>
2026-07-31 12:02:30 +0200

    Android 4.0.1

--- buildSrc/src/main/kotlin/ConfigData.kt
+++ buildSrc/src/main/kotlin/ConfigData.kt
@@ -2,8 +2,8 @@ import org.gradle.api.JavaVersion
 
 object ConfigData {
     //android
-    const val versionCode = 11
-    const val versionName = "4.0.0"
+    const val versionCode = 12
+    const val versionName = "4.0.1"
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     val javaVersion = JavaVersion.VERSION_21
 }
